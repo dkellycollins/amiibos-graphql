eae6fcb1b2bcd47ad88b914905cc8496aebafb8b
Devin Kelly-Collins <devin.kellycollins@artisantechgroup.com>
2018-07-19 20:36:56 -0500

    Updated index.ts with graphql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+import * as express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const typeDefs   = gql(`
+type Query {
+  message: String
+}
+
+schema {
+  query: Query
+}`);
+
+const resolvers = {
+  Query: {
+    message: () => 'Hello World!'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
+server.applyMiddleware({ app, cors: true });
+app.listen(3000, () => console.log('Listening on port 3000 :)'));
