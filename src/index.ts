0dc814f714f73cba883a37ddf62325158d4ab60b
Devin Kelly-Collins <devin.kellycollins@artisantechgroup.com>
2018-07-25 21:26:28 -0500

    Added amiibo query

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
 import * as express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
-const typeDefs   = gql(`
+const data = [
+  { name: 'mario_0', display: 'Mario', releaseDate: '1970-01-01', series: { name: 'super_mario_bros', display: 'Super Mario Bros.'}}
+];
+
+const typeDefs   = gql`
+type Amiibo {
+  name: String!
+  display: String!
+  releaseDate: String
+  series: AmiiboSeries
+}
+
+type AmiiboSeries {
+  name: String!
+  display: String!
+}
+
 type Query {
   message: String
+  amiibos: [Amiibo]
+  amiibo(name: String!): Amiibo
 }
 
 schema {
   query: Query
-}`);
+}`;
 
 const resolvers = {
   Query: {
-    message: () => 'Hello World!'
+    message: () => 'Hello World!',
+    amiibos: () => data,
+    amiibo: (obj: any, args: any) => data.filter(i => i.name === args.name)
   }
 };
 
